e7858490a8d970bea9264c03af4aeccfaa6eefe0
azrilpramudia <azrilpramudia01@gmail.com>
2025-07-26 05:25:07 +0700

    feat: add data

---
+++ src/data/skillsData.js
@@ -0,0 +1,90 @@
+import html from "/html.png";
+import css from "/css.png";
+import js from "/js.png";
+import node from "/nodejs.png";
+import react from "/react.png";
+import tailwind from "/tailwind.png";
+import git from "/git.png";
+import github from "/github.png";
+import mysql from "/mysql.png";
+import postman from "/postman.png";
+import linux from "/linux.png";
+import figma from "/figma.png";
+import astronaut from "/astronaut.png";
+
+export const skillsData = [
+  {
+    src: html,
+    label: "HTML",
+    border: "border-orange-500",
+    shadow: "shadow-orange-400",
+  },
+  {
+    src: css,
+    label: "CSS",
+    border: "border-blue-500",
+    shadow: "shadow-blue-400",
+  },
+  {
+    src: js,
+    label: "JavaScript",
+    border: "border-yellow-400",
+    shadow: "shadow-yellow-300",
+  },
+  {
+    src: node,
+    label: "Node.js",
+    border: "border-green-500",
+    shadow: "shadow-green-400",
+  },
+  {
+    src: react,
+    label: "React",
+    border: "border-cyan-400",
+    shadow: "shadow-cyan-300",
+  },
+  {
+    src: tailwind,
+    label: "Tailwind",
+    border: "border-teal-400",
+    shadow: "shadow-teal-300",
+  },
+  {
+    src: git,
+    label: "Git",
+    border: "border-red-500",
+    shadow: "shadow-red-400",
+  },
+  {
+    src: github,
+    label: "GitHub",
+    border: "border-white",
+    shadow: "shadow-white",
+  },
+  {
+    src: mysql,
+    label: "MySQL",
+    border: "border-blue-300",
+    shadow: "shadow-blue-200",
+  },
+  {
+    src: postman,
+    label: "Postman",
+    border: "border-orange-400",
+    shadow: "shadow-orange-300",
+  },
+  {
+    src: linux,
+    label: "Linux",
+    border: "border-gray-400",
+    shadow: "shadow-gray-300",
+  },
+  {
+    src: figma,
+    label: "Figma",
+    border: "border-green-400",
+    shadow: "shadow-green-300",
+  },
+];
+
+export { astronaut };
