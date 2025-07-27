3d6c4d21ecb44199db6913236212a2aaf7ae4084
azrilpramudia <azrilpramudia01@gmail.com>
2025-07-28 04:15:37 +0700

    feat: add hire me

--- src/pages/ProjectsPages.jsx
+++
@@ -1,9 +0,0 @@
-const ProjectsPages = () => {
-  return (
-    <div className="min-h-screen bg-primary flex justify-center items-center">
-      <h1 className="font-poppins text-gray-500 font-bold">Project</h1>
-    </div>
-  );
-};
-
-export default ProjectsPages;
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,13 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePages";
-import ProjectsPages from "../pages/ProjectsPages";
 import HireMe from "../pages/Hire-Me";
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/projects" element={<ProjectsPages />} />
       <Route path="/hire-me" element={<HireMe />} />
     </Routes>
   );
